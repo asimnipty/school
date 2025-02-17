4bb02e24dc5de5ccdcc76fe9566ec927cbf37254
asimnipty <asimnipty@yahoo.com>
2025-02-17 22:34:47 +0600

    nav md

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 p-4 shadow-lg sticky">
-      <div className="container text-yellow-500 font-bold mx-auto flex justify-between items-center">
+    <nav className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 p-4 shadow-lg text-xl text-lime-900 sticky">
+      <div className="container font-bold mx-auto flex justify-between items-center">
         {/* Logo */}
         <Link to="/">
-          <img className=" rounded-full size-[80px] shadow-amber-500 shadow-lg" src={logo} alt="" />
+          <img className="rounded-full size-[80px] shadow-amber-500 shadow-lg" src={logo} alt="" />
         </Link>
 
         {/* Desktop Menu */}
@@ -44,12 +44,13 @@ const Navbar = () => {
 
       {/* Mobile Menu Dropdown */}
       {isOpen && (
-        <div className="md:hidden bg-indigo-300 font-bold text-red-400 rounded-md text-center space-y-3 p-4">
-          <Link to="/" className="block texxt-bold hover:text-gray-200">Home</Link>
-          <Link to="/about" className="block hover:text-gray-200">About</Link>
-          <Link to="/contact" className="block hover:text-gray-200">Contact</Link>
-          <Link to="/gallary" className="block hover:text-gray-200">Gallary</Link>
-          <hr className="border-gray-400" />
+        
+        <div className="md:hidden w-auto text-xl font-bold rounded-md text-center space-y-3 p-4">
+          <Link to="/" className="block text-bold hover:text-gray-200 shadow-md">Home</Link>
+          <Link to="/about" className="block hover:text-gray-200 shadow-md">About</Link>
+          <Link to="/contact" className="block hover:text-gray-200 shadow-md">Contact</Link>
+          <Link to="/gallary" className="block hover:text-gray-200 shadow-md">Gallary</Link>
+          {/* <hr className="border-gray-400" /> */}
           {/* <Link to="/login" className="block bg-white text-blue-600 px-4 py-2 rounded-md hover:bg-gray-100">
             Login
           </Link> */}
